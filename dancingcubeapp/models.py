b695a6b3517aa101cc22ba617f63aec3d67e54bf
julien.linder <julien.linder@he-arc.ch>
2020-02-19 17:15:43 +0100

    j'ai fait fonctionner la migration mais c'est pas propre

--- dancingcubeapp/models.py
+++ dancingcubeapp/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
-# Create your models here.
-
 class Map(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, default='')
+    music = models.CharField(max_length=200, default='')
+    EASY = '1'
+    MEDIUM = '2'
+    HARD = '3'
+    diff_rates = [
+        (EASY, 'easy'),
+        (MEDIUM, 'medium'),
+        (HARD, 'hard'),
+    ]
+    difficulty = models.CharField(
+        max_length=1,
+        choices=diff_rates,
+        default=EASY
+        )
+    uploader = models.CharField(max_length=200, default='')
+    map = models.FileField(upload_to='media/maps/', default='rien.txt')
 
     def __str__(self):
-        return self.name
+        return self.title
