bf150b63c01caf8d1ff591fc1d3e957078583227
julienlinder <julien.linder@he-arc.ch>
2020-02-26 17:17:04 +0100

    debut de l'upload/creation de map

--- dancingcubeapp/models.py
+++ dancingcubeapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Map(models.Model):
     name = models.CharField(max_length=30, default='')
-    music = models.CharField(max_length=200, default='')
+    music = models.FileField(upload_to='media/musics/', blank=True)
     EASY = '1'
     MEDIUM = '2'
     HARD = '3'
@@ -16,8 +16,9 @@ class Map(models.Model):
         choices=diff_rates,
         default=EASY
         )
+    image = models.ImageField(upload_to='media/images/', blank=True)
     uploader = models.CharField(max_length=200, default='')
-    map = models.FileField(upload_to='media/maps/', default='rien.txt')
+    map = models.FileField(upload_to='media/maps/', blank=True)
 
     def __str__(self):
-        return self.title
+        return self.name
