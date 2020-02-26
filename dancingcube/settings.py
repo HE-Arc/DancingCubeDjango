af177d3954b0ea50146387cec2c43a7fa5fc9f07
julienlinder <julien.linder@he-arc.ch>
2020-02-26 16:39:44 +0100

    update settings (même si on vois rien)

--- dancingcube/settings.py
+++ dancingcube/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'dancingcube.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dancingcubedb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        },
+    'default_sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 
