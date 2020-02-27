147c3281b9cc627c7c02bfc02464f14d71ac7226
ergun.kilic <ergun.kilic@he-arc.ch>
2020-02-27 14:37:15 +0100

    Default login interface Static folder for CSS

--- dancingcube/settings.py
+++ dancingcube/settings.py
@@ -32,14 +32,18 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'dancingcubeapp.apps.DancingcubeappConfig',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -128,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/dancingcubeapp/'
--- dancingcubeapp/urls.py
+++ dancingcubeapp/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard', views.DashboardView.as_view(), name='dashboard'),
-
     path('dashboard/maps', views.MapListView.as_view(), name='dashboard-maps'),
     path('dashboard/maps/<pk>/', views.MapDetailView.as_view(), name='map-detail'),
 ]
