46d00900fd09cc756c2c80d5f741057a8f78e414
ourfbht <ourfbht@gmail.com>
2020-03-12 14:23:11 +0100

    for deploy

--- dancingcube/settings.py
+++ dancingcube/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mrev!-v0&n@5^6_n4*zkr+0m1_pgu2!0o#spu@cs+7_3db7*6d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dancing3.srvz-webapp.he-arc.ch']
 
 
 # Application definition
@@ -93,6 +93,22 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 }
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('GROUPNAME'),
+        'USER': os.environ.get('GROUPNAME', 'root'),
+        'PASSWORD': os.environ.get('PASSWORD', ''),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4'
+        }
+    }
+}
+"""
 
 
 # Password validation
